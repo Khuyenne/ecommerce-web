40881438374a90d5c49c042857eb8455d6783f0a
Khuyenne <kbui7272@gmail.com>
2024-02-28 16:08:42 +0700

    update shop page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ function App() {
   return (
     <>
       <NavItems/>
+      {/* relative to the viewport: min-height 100vh */}
+      <div className="min-vh-100"> 
       <Outlet/>
+      </div>
       <Footer/>
     </>
   )
---
+++ src/shop/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../compoments/PageHeader'
+
+const Shop = () => {
+  return (
+    <div>
+        <PageHeader/>
+    </div>
+  )
+}
+
+export default Shop
