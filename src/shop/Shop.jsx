d49abc4e77583f0f568787a3616385be263fd350
Khuyenne <kbui7272@gmail.com>
2024-02-29 16:01:06 +0700

    update shoppage

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from '../compoments/PageHeader'
 
+const showResults ="Showing 01 - 12 of 139 Results"
+
 const Shop = () => {
+  const [GridList, setGridList] = useState(true);
+
   return (
     <div>
-        <PageHeader/>
+        <PageHeader title="Our Shop Page" curPage="Shop"/>
+
+        {/** shop page */}
+        <div className="shop-page padding-tb">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 col-12">
+                <article>
+                  <div className="shop-title d-flex flex-warp justify-content-between">
+                    <p>{showResults}</p>
+                    <div className={`product-view-mode ${GridList ? "gridActive" : 'listActive'}`}>
+                      <a className="grid" onClick={() => setGridList(!GridList)}>
+                        <i className="icofont-ghost"></i>
+                      </a>
+                      <a className="list" onClick={() => setGridList(!GridList)}>
+                        <i className="icofont-listine-dots"></i>
+                      </a>
+
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div className="col-lg-4 col-12">right side</div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
