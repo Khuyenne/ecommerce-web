455c13660277cab873061df0a18a96198a8a32c9
Khuyenne <kbui7272@gmail.com>
2024-03-06 20:04:44 +0700

    update

--- src/shop/ProductDisplay.jsx
+++ src/shop/ProductDisplay.jsx
@@ -8,6 +8,13 @@ const ProductDisplay = ({item}) => {
     const [coupon, setCoupon] = useState("");
     const [size, setSize] = useState("Select Size");
     const [color, setColor]= useState("Select Color");
+    const handleSizeChange = (e) => {
+      setSize(e.target.value);
+    }
+
+    const handleColorChange = (e) => {
+      setColor(e.target.value);
+    }
 
   return (
     <div>
@@ -24,6 +31,45 @@ const ProductDisplay = ({item}) => {
             <h4>${price}</h4>
             <h6>{seller}</h6>
             <p>{desc}</p>
+        </div>
+
+        {/** cart compoments */}
+        <div>
+          <form>
+            {/** size */}
+            <div className="select-product size">
+              <select value={size} onChange={handleSizeChange}>
+                <option>Select Size</option>
+                <option>SM</option>
+                <option>MD</option>
+                <option>LG</option>
+                <option>XL</option>
+                <option>XXL</option>
+              </select>
+              <i className="icofont-rounded-down"></i>
+            </div>
+
+            {/** color */}
+            <div className="select-product color">
+              <select value={color} onChange={handleColorChange}>
+                <option>Select Color</option>
+                <option>Pink</option>
+                <option>Ash</option>
+                <option>Red</option>
+                <option>White</option>
+                <option>Blue</option>
+                <option>Black</option>
+              </select>
+              <i className="icofont-rounded-down"></i>
+            </div>
+
+            {/** cart plus minus */}
+            <div className="cart-plus-minus">
+              <div>-</div>
+              <input type="text"  name='qtybutton' id='qtybutton' value={prequantity}/>
+              <div>+</div>
+            </div>
+          </form>
         </div>
     </div>
   )
